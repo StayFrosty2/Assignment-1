a7a032f3a3e0b6f02d534a0a93a237bb15925a07
Claire <cameron.liam.yon@gmail.com>
2025-11-01 20:59:02 +0000

    testing menu, finished head

--- menu.php
+++ menu.php
@@ -1,5 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-
+        <title>The Meadough Pizzaria | Menu</title>
+        <meta charset="utf-8" />
+        <meta name="author" content="Cameron Yon-Vachon" />
+        <meta name="description" content="The Menu for The Meadough Pizzaria" />
+        <link rel="shortcut icon" href="images/pizza.svg" type="image/x-icon" />
+        <link rel="stylesheet" href="styles/styles.css" />
     </head>
+    <body>
+        <p>hello :3</p>
+    </body>

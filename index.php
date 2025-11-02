d7ecfa81eab6f5fc4c9e8d3ef3d3fc06a48f2ff6
Claire <cameron.liam.yon@gmail.com>
2025-11-02 22:04:48 +0000

    menu started, index finally fixed

--- index.php
+++ index.php
@@ -20,37 +20,24 @@
         <main>
             <section>
                 <h2>Locations</h2>
-                <ul>
-                    <li><p>Barrie, 402 Undermeadow Dr</p>
-                    <img src="images/pizzaria 1.png" height="512" width="512" alt="Barrie Location" /></li>
-
-                    <li><p>Penetanguishine, 54 Yonc Ave</p>
-                    <img src="images/the meadough resturant.png" height="384" width="512" alt="Penetanguishine Location" /></li>
-                </ul>
+                <p>Barrie, 402 Undermeadow Dr</p>
+                <img src="images/pizzaria 1.png" height="512" width="512" alt="Barrie Location" />
+                <p>Penetanguishine, 54 Yonc Ave</p>
+                <img src="images/the meadough resturant.png" height="384" width="512" alt="Penetanguishine Location" />
             </section>
             <section>
                 <h2>Popular Items</h2>
-                <ul>
-                    <li>
-                        <h3>Classic Medium Woodfire Pepperoni: $10.20</h3>
-                        <img src="images/pepperoni-pizza.svg" height="200" width="200" alt="Pepperoni Pizza" />
-                        <p>A Pepperonni Pizza, baked in our woodfire ovens. A homemade taste with <em>lots</em> of love.</p>
-                    </li>
-                    <li>
-                        <h3>Deluxe BBQ Chicken: $18.99</h3>
-                        <img src="images/bbq-pizza.svg" height="200" width="200" alt="BBQ Pizza" />
-                        <p>The biggest and bestest we can make, rotisserie styled shredded chicken and red pepper, drizzled in our homemade buffalo-style BBQ sauce.</p>
-                    </li>
-                    <li>
-                        <h3>Specialty Cheesy Breadsticks: $5.45</h3>
-                        <img src="images/breadsticks.svg" height="150" width="200" alt="Breadsticks" />
-                        <p>A recipe past down through the smith family for decades, these breadsticks have generations worth of tests and a number of 5 star reviewers (and not all just our aunts and uncles!)</p>
-                    </li>
-                    <li>
-                        <h3><em>We also do "Build your own" custom Pizzas!</em></h3>
-                        <p>3 Sizes (Small $7.99, Medium $12.99, Large $17.99), Up to 3 toppings ($1.00 for each additional topping), several kinds of cheese and sauce.</p>
-                    </li>
-                </ul>
+                <h3>Classic Medium Woodfire Pepperoni: $10.20</h3>
+                <img src="images/pepperoni-pizza.svg" height="200" width="200" alt="Pepperoni Pizza" />
+                <p>A Pepperonni Pizza, baked in our woodfire ovens. A homemade taste with <em>lots</em> of love.</p>
+                <h3>Deluxe BBQ Chicken: $18.99</h3>
+                <img src="images/bbq-pizza.svg" height="200" width="200" alt="BBQ Pizza" />
+                <p>The biggest and bestest we can make, rotisserie styled shredded chicken and red pepper, drizzled in our homemade buffalo-style BBQ sauce.</p>
+                <h3>Specialty Cheesy Breadsticks: $5.45</h3>
+                <img src="images/breadsticks.svg" height="150" width="200" alt="Breadsticks" />
+                <p>A recipe past down through the smith family for decades, these breadsticks have generations worth of tests and a number of 5 star reviewers (and not all just our aunts and uncles!)</p>
+                <h3><em>We also do "Build your own" custom Pizzas!</em></h3>
+                <p>3 Sizes (Small $7.99, Medium $12.99, Large $17.99), Up to 3 toppings ($1.00 for each additional topping), several kinds of cheese and sauce.</p>
                 <h3>Interested?</h3>
                 <p><a href="menu.php">See the menu and Order Here!</a></p>
             </section>
--- menu.php
+++ menu.php
@@ -6,7 +6,7 @@
         <meta name="author" content="Cameron Yon-Vachon" />
         <meta name="description" content="The Menu for The Meadough Pizzaria" />
         <link rel="shortcut icon" href="images/pizza.svg" type="image/x-icon" />
-        <link rel="stylesheet" href="styles/styles.css" />
+        <link rel="stylesheet" href="styles/menu-style.css" />
     </head>
     <body>
         <header>
@@ -19,7 +19,16 @@
             <h1>Menu</h1>
         </header>
         <main>
-            <p>hello :3</p>
+            <img src="images/pepperoni-pizza.svg" height="200" width="200" alt="Pepperoni Pizza" />
+            <h3>Classic Woodfire Pepperoni</h3>
+            <input type="radio" name="pepperoni" id="pepperoni" value="pepperoni" />
+            <select name="pepsize" id="pepsize">
+                <optgroup label="Size">
+                    <option value="small">Small $6.99</option>
+                    <option value="medium">Medium $10.20</option>
+                    <option value="large">Large $14.45</option>
+                </optgroup>
+            </select>
         </main>
         <footer>
             <?php
